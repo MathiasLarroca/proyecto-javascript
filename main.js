18774e6722873ddacfe88ff2ff174d9b2632cc5b
MathiasLarroca <mathiaslarrocagonzalez@gmail.com>
2024-02-22 23:11:54 -0300

    Configurando variaciones de ahorcado

---
+++ main.js
@@ -0,0 +1,323 @@
+const RESPUESTA = "C A R S"
+
+// function equivocar() {
+    
+//     if (i == 1) {
+
+//         alert("Te quedaste sin intentos! Que lastima.")
+
+//     } else {
+
+//         alert("Te equivocaste te quedan " + (i - 1) + " intentos")
+
+//     }
+    
+// }
+
+for (let i = 4; i > 0; i--) {
+
+    let letra = prompt("Adivina una letra de la pelicula! Puedes equivocarte 4 veces: _ _ _ _").toLowerCase()
+
+    if (letra != "c" && letra != "a" && letra != "r" && letra != "s") {
+
+        if (i == 1) {
+
+            alert("Te quedaste sin intentos! Que lastima.")
+
+        } else {
+
+            alert("Te equivocaste te quedan " + (i - 1) + " intentos")
+
+        }
+
+    } else if (letra == "c") {
+
+        for (i = i; i > 0; i--) {
+
+            letra = prompt("Adivinaste la C! Adivina otra letra: C _ _ _").toLowerCase()
+
+            if (letra != "c" && letra != "a" && letra != "r" && letra != "s") {
+
+                if (i == 1) {
+        
+                    alert("Te quedaste sin intentos! Que lastima.")
+
+        
+                } else {
+        
+                    alert("Te equivocaste te quedan " + (i - 1) + " intentos")
+        
+                }
+        
+            } else if (letra == "a") {
+
+                for (i = i; i > 0; i--) {
+
+                    letra = prompt("Adivinaste la A! Adivina otra letra: C A _ _").toLowerCase()
+
+            if (letra != "c" && letra != "a" && letra != "r" && letra != "s") {
+
+                if (i == 1) {
+        
+                    alert("Te quedaste sin intentos! Que lastima.")
+
+        
+                } else {
+        
+                    alert("Te equivocaste te quedan " + (i - 1) + " intentos")
+        
+                }
+        
+            } else if (letra == "r") {
+
+                for (i = i; i > 0; i--) {
+
+                    letra = prompt("Adivinaste la R! Adivina otra letra: C A R _").toLowerCase()
+
+            if (letra != "c" && letra != "a" && letra != "r" && letra != "s") {
+
+                if (i == 1) {
+        
+                    alert("Te quedaste sin intentos! Que lastima.")
+
+        
+                } else {
+        
+                    alert("Te equivocaste te quedan " + (i - 1) + " intentos")
+        
+                }
+        
+            } else if (letra == "s") {
+
+                alert("Adivinaste la pelicula!: " + RESPUESTA)
+
+                i = 0
+
+                }
+
+            }
+
+                
+
+            } else if (letra == "s") {
+
+                for (i = i; i > 0; i--) {
+
+                    letra = prompt("Adivinaste la S! Adivina otra letra: C A _ S").toLowerCase()
+
+            if (letra != "c" && letra != "a" && letra != "r" && letra != "s") {
+
+                if (i == 1) {
+        
+                    alert("Te quedaste sin intentos! Que lastima.")
+
+        
+                } else {
+        
+                    alert("Te equivocaste te quedan " + (i - 1) + " intentos")
+        
+                }
+        
+            } else if (letra == "r") {
+
+                alert("Adivinaste la pelicula!: " + RESPUESTA)
+
+                i = 0
+
+            }
+
+                }
+
+            }
+
+                }
+            } else if (letra == "r") {
+
+                for (i = i; i > 0; i--){
+
+                    letra = prompt("Adivinaste la R! Adivina otra letra: C _ R _").toLowerCase()
+
+            if (letra != "c" && letra != "a" && letra != "r" && letra != "s") {
+
+                if (i == 1) {
+        
+                    alert("Te quedaste sin intentos! Que lastima.")
+
+        
+                } else {
+        
+                    alert("Te equivocaste te quedan " + (i - 1) + " intentos")
+        
+                }
+        
+            } else if (letra == "s") {
+
+                for (i = i; i > 0; i--) {
+
+                    letra = prompt("Adivinaste la S! Adivina otra letra: C _ R S").toLowerCase()
+
+            if (letra != "c" && letra != "a" && letra != "r" && letra != "s") {
+
+                if (i == 1) {
+        
+                    alert("Te quedaste sin intentos! Que lastima.")
+
+        
+                } else {
+        
+                    alert("Te equivocaste te quedan " + (i - 1) + " intentos")
+        
+                }
+        
+            } else if (letra == "a") {
+
+                alert("Adivinaste la pelicula!: " + RESPUESTA)
+
+                i = 0
+
+            }
+
+                }
+
+            } else if (letra == "a") {
+
+                for (i = i; i > 0; i--) {
+
+                    letra = prompt("Adivinaste la A! Adivina otra letra: C A R _").toLowerCase()
+
+            if (letra != "c" && letra != "a" && letra != "r" && letra != "s") {
+
+                if (i == 1) {
+        
+                    alert("Te quedaste sin intentos! Que lastima.")
+
+        
+                } else {
+        
+                    alert("Te equivocaste te quedan " + (i - 1) + " intentos")
+        
+                }
+        
+            } else if (letra == "s") {
+
+                alert("Adivinaste la pelicula!: " + RESPUESTA)
+
+                i = 0
+
+            }
+
+                }
+
+            }
+
+                }
+
+            } else if (letra == "s") {
+
+                for (i = i; i > 0; i--) {
+
+                    letra = prompt("Adivinaste la S! Adivina otra letra: C _ _ S").toLowerCase()
+
+            if (letra != "c" && letra != "a" && letra != "r" && letra != "s") {
+
+                if (i == 1) {
+        
+                    alert("Te quedaste sin intentos! Que lastima.")
+
+        
+                } else {
+        
+                    alert("Te equivocaste te quedan " + (i - 1) + " intentos")
+        
+                }
+        
+            } else if (letra == "a") {
+
+                for (i = i; i > 0; i--) {
+
+                    letra = prompt("Adivinaste la A! Adivina otra letra: C A _ S").toLowerCase()
+
+            if (letra != "c" && letra != "a" && letra != "r" && letra != "s") {
+
+                if (i == 1) {
+        
+                    alert("Te quedaste sin intentos! Que lastima.")
+
+        
+                } else {
+        
+                    alert("Te equivocaste te quedan " + (i - 1) + " intentos")
+        
+                }
+        
+            } else if (letra == "r") {
+
+                alert("Adivinaste la pelicula!: " + RESPUESTA)
+
+                i = 0
+
+            }
+
+                }
+
+            } else if (letra == "r") {
+
+                for (i = i; i > 0; i--) {
+
+                    letra = prompt("Adivinaste la R! Adivina otra letra: C _ R S").toLowerCase()
+
+            if (letra != "c" && letra != "a" && letra != "r" && letra != "s") {
+
+                if (i == 1) {
+        
+                    alert("Te quedaste sin intentos! Que lastima.")
+
+        
+                } else {
+        
+                    alert("Te equivocaste te quedan " + (i - 1) + " intentos")
+        
+                }
+        
+            } else if (letra == "a") {
+
+                alert("Adivinaste la pelicula!: " + RESPUESTA)
+
+                i = 0
+
+            }
+
+                }
+
+            }
+
+                }
+
+            }
+                }
+
+            } else if (letra == "a") {
+
+                for (i = i; i > 0; i--) {
+
+                    letra = prompt("Adivinaste la A! Adivina otra letra: _ A _ _").toLowerCase()
+
+            if (letra != "c" && letra != "a" && letra != "r" && letra != "s") {
+
+                if (i == 1) {
+        
+                    alert("Te quedaste sin intentos! Que lastima.")
+
+        
+                } else {
+        
+                    alert("Te equivocaste te quedan " + (i - 1) + " intentos")
+        
+                }
+        
+            }
+
+                }
+
+            }
+        }
