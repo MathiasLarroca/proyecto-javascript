d47a0e1fb0c457b4d1555a5cf131946429e9c914
MathiasLarroca <mathiaslarrocagonzalez@gmail.com>
2024-03-21 01:33:20 -0300

    Configurando todas las posibilidades para el juego

--- main.js
+++ main.js
@@ -8,58 +8,57 @@ let letraAdivinada
 
 let seguirJugando = true
 
-while (seguirJugando) {
+function adivinar() {
 
-    if (confirm("Quieres ingresar una respuesta personalizada para un amigue?") == true) {
+    for (let l = 6; l > 0; l--) {
 
-        respuesta = prompt("Ingrese su respuesta.\nNo olvides de decirle a tu amigue que categoria es!").toUpperCase()
+        let letraCorrecta = true
 
-    } else {
+        while (letraCorrecta) {
 
-        alert("Una respuesta aleatoria sera dada.")
+            letraAdivinada = prompt("Tienes " + l + " intentos\n\nPor favor ingrese una letra\n\n" + respuestaAdivinada.split(" ").map(palabra => palabra.split("").join(" ")).join("  ")).toUpperCase()
 
-        for (let i = RESPUESTAS.length; i > 0; i--) {
+            if (respuestaAdivinada.includes(letraAdivinada)) {
 
-            //No se por que, pero si guardo "k" con const o let me dice que queda undefined. Aparte de esto, el algoritmo no esta funcionando
+                alert("Ya adivinaste esa letra. Elige otra.")
 
-            for (let j = RESPUESTAS.length - 1; j > 0; j--) {
-                var k = Math.floor(Math.random() * (j + 1))
-                [RESPUESTAS[j], RESPUESTAS[k]] = [RESPUESTAS[k], RESPUESTAS[j]]
-            }
+            } else if (respuesta.includes(letraAdivinada)) {
 
-            respuesta = RESPUESTAS.pop()
+                for (let m = 0; m < respuesta.length; m++) {
 
-            respuestaAdivinada = respuesta.replace(/\S/g, "_").trim()
+                    if (respuesta[m] === letraAdivinada) {
 
-            for (let l = 6; l > 0; l--) {
-
-                let letraCorrecta = true
-
-                while (letraCorrecta) {
-
-                    letraAdivinada = prompt("Tienes " + l + " intentos\n\nPor favor ingrese una letra\n\n" + respuestaAdivinada.split(" ").map(palabra => palabra.split("").join(" ")).join("  ")).toUpperCase()
-
-                    if (respuesta.includes(letraAdivinada)) {
-
-                        for (let m = 0; m < respuesta.length; m++) {
-
-                            if (respuesta[m] === letraAdivinada) {
-
-                                respuestaAdivinada = respuestaAdivinada.substring(0, m) + letraAdivinada + respuestaAdivinada.substring(m + 1)
-
-                            }
-
-                        }
-
-                        alert("Adivinaste la letra " + letraAdivinada + "! Siga asi!")
-
-                    } else{
-
-                        alert("Esa letra era incorrecta! Intente otra.")
-
-                        letraCorrecta = false
+                        respuestaAdivinada = respuestaAdivinada.substring(0, m) + letraAdivinada + respuestaAdivinada.substring(m + 1)
 
                     }
+
+                }
+
+                if (respuestaAdivinada == respuesta) {
+
+                    alert("Felicidades! Adivinaste la respuesta!\n\n" + respuesta)
+
+                    l = 0
+
+                    break
+
+                }
+
+                alert("Adivinaste la letra " + letraAdivinada + "! Siga asi!")
+
+            } else {
+
+                if (l == 1) {
+
+                    alert("Te quedaste sin intentos. La respuesta correcta era " + respuesta)
+
+                    letraCorrecta = false
+
+                } else {
+
+                    alert("Esa letra era incorrecta! Intente otra.")
+
+                    letraCorrecta = false
 
                 }
 
@@ -69,27 +68,46 @@ while (seguirJugando) {
 
     }
 
+}
 
+alert("Bienvenido al juego del Ahorcado!")
 
+while (seguirJugando) {
 
+    if (confirm("Quieres ingresar una respuesta personalizada para un amigue?") == true) {
 
+        respuesta = prompt("Ingrese su respuesta.\nNo olvides de decirle a tu amigue que categoria es!").toUpperCase()
 
+        respuestaAdivinada = respuesta.replace(/\S/g, "_").trim()
 
+        adivinar()
 
+    } else {
 
+        if (RESPUESTAS.length == 0) {
 
+            alert("Ya utilizaste todas las respuestas disponibles. Intente con una personalizada")
 
+        } else {
 
+            alert("Una pelicula aleatoria sera dada como respuesta.")
 
+                //No se por que, pero si guardo "k" con const o let me dice que queda undefined. Aparte de esto, el algoritmo no esta funcionando
 
+                for (let j = RESPUESTAS.length - 1; j > 0; j--) {
+                    var k = Math.floor(Math.random() * (j + 1))
+                    [RESPUESTAS[j], RESPUESTAS[k]] = [RESPUESTAS[k], RESPUESTAS[j]]
+                }
 
+                respuesta = RESPUESTAS.pop()
 
+                respuestaAdivinada = respuesta.replace(/\S/g, "_").trim()
 
+                adivinar()
 
+        }
 
-
-
-
+    }
 
     if (confirm("Desea seguir jugando?") == false) {
 
